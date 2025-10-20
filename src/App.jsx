2982a7b890bebbb3eed569cb095918bbcda9e78c
Mohamad Ismail Hossain Siddiquee <73274592+Ismail-Ai404@users.noreply.github.com>
2025-10-20 10:44:23 +0600

    Add toggle for the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 	function generateAllNewDice() {
 		return new Array(10).fill(0).map(() => ({
 			value: Math.ceil(Math.random() * 6),
-			isHeld: true,
+			isHeld: false,
 			id: nanoid(),
 		}));
 	}
@@ -19,8 +19,21 @@ export default function App() {
 		setDice(generateAllNewDice());
 	}
 
+	function hold(id) {
+		setDice((oldDice) =>
+			oldDice.map((die) =>
+				die.id === id ? { ...die, isHeld: !die.isHeld } : die
+			)
+		);
+	}
+
 	const diceElements = dice.map((dieObj) => (
-		<Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+		<Die
+			key={dieObj.id}
+			value={dieObj.value}
+			isHeld={dieObj.isHeld}
+			hold={() => hold(dieObj.id)}
+		/>
 	));
 
 	return (
